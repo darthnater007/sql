fc12e43140a824a7da25ac3ca006ee4849b44b17
darthnater007 <nsperk@gmail.com>
2018-03-07 11:28:43 -0500

    Added additional products to Table

--- mma/mma_create_database.sql
+++ mma/mma_create_database.sql
@@ -12,14 +12,20 @@ CREATE TABLE Product (
 );
 
 -- insert some rows into the Product table
-INSERT INTO Product VALUES
-(1, 'java', 'Murach''s Java Programming', '57.50'),
-(2, 'jsp', 'Murach''s Java Servlets and JSP', '57.50'),
-(3, 'mysql', 'Murach''s MySQL', '54.50'),
-(4, 'android', 'Murach''s Android Programming', '57.50'),
-(5, 'html5', 'Murach''s HTML5 and CSS3', '54.50'),
-(6, 'oracle', 'Murach''s Oracle and PL/SQL', '54.50'),
-(7, 'javascript', 'Murach''s JavaScript and jQuery', '54.50');
+INSERT INTO `product` (Code, Description, ListPrice) VALUES
+('java','Murach\'s Java Programming',57.50),
+('jsp','Murach\'s Java Servlets and JSP',57.50),
+('mysql','Murach\'s MySQL',54.50),
+('android','Murach\'s Android Programming',57.50),
+('html5','Murach\'s HTML5 and CSS3',54.50),
+('oracle','Murach\'s Oracle and PL/SQL',54.50),
+('javascript','Murach\'s JavaScript and jQuery',57.50),
+('ASP.net','Murach\'s ASP.net',58.00),
+('php','Murach\'s PHP',57.50),
+('perl','Murach\'s Perl',57.50),
+('ruby','Murach\'s Ruby',59.00),
+('natecode','Murach\'s NateCode',435.00),
+('test','test book',10.00);
 
 -- create a user and grant privileges to that user
 GRANT SELECT, INSERT, DELETE, UPDATE

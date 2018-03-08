25806468fedd4b6895b7e12d1e2ede96d7454ded
darthnater007 <nsperk@gmail.com>
2018-03-08 14:20:47 -0500

    Added Invoice and LineItem tables, along with basic Inserts of records

--- mma/mma_create_database.sql
+++ mma/mma_create_database.sql
@@ -5,14 +5,32 @@ USE mma;
 
 -- create the Product table
 CREATE TABLE Product (
-  ProductID      INT            PRIMARY KEY  AUTO_INCREMENT,
+  ID      		 INT            PRIMARY KEY  AUTO_INCREMENT,
   Code           VARCHAR(10)    NOT NULL     UNIQUE,
   Description    VARCHAR(255)   NOT NULL,
   ListPrice      DECIMAL(10,2)  NOT NULL
 );
 
+CREATE TABLE Invoice (
+ID				INT 			PRIMARY KEY 	AUTO_INCREMENT,
+OrderNumber 	VARCHAR(25)		NOT NULL	UNIQUE,
+CustomerName	VARCHAR(50) 	NOT NULL,
+OrderDate		DATE 			NOT NULL,
+TOTAL			DECIMAL(10,2) 	NOT NULL
+);
+
+CREATE TABLE LineItem (
+ID			INT 	PRIMARY KEY 	AUTO_INCREMENT,
+InvoiceID 	INT		NOT NULL,
+ProductID	INT 	NOT NULL,
+Quantity 	INT 	NOT NULL,
+
+FOREIGN KEY (InvoiceID) REFERENCES Invoice (ID),
+FOREIGN KEY (ProductID) REFERENCES Product (ID)
+);
+
 -- insert some rows into the Product table
-INSERT INTO `product` (Code, Description, ListPrice) VALUES
+INSERT INTO Product (Code, Description, ListPrice) VALUES
 ('java','Murach\'s Java Programming',57.50),
 ('jsp','Murach\'s Java Servlets and JSP',57.50),
 ('mysql','Murach\'s MySQL',54.50),
@@ -26,6 +44,13 @@ INSERT INTO `product` (Code, Description, ListPrice) VALUES
 ('ruby','Murach\'s Ruby',59.00),
 ('natecode','Murach\'s NateCode',435.00),
 ('test','test book',10.00);
+
+INSERT INTO Invoice (OrderNumber, CustomerName, OrderDate, Total) VALUES
+('NP0001', 'Nate Perkins', '20190308', '0');
+
+INSERT INTO LineItem (InvoiceID, ProductID, Quantity) VALUES
+('1', '7', '5')
+('1', '5', '10');
 
 -- create a user and grant privileges to that user
 GRANT SELECT, INSERT, DELETE, UPDATE
